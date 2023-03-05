bfc26fd1743ecdc6d5ab345b34201b49beb81c40
Saksham Singh <saksham5904@gmail.com>
2023-03-05 20:22:42 +0530

    chapter5

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 
@@ -8,56 +8,60 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        // ignore: prefer_const_literals_to_create_immutables
-        children: [
-          SizedBox(
-            height: 100,
-          ),
-          Image.asset("assets/images/login.png",fit: BoxFit.cover,),
-          SizedBox(
-            height: 60,
-          ),
-          Text(
-              "Welcome",
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.pink,
+      child: SingleChildScrollView(
+        child: Column(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            Image.asset("assets/images/login.png",fit: BoxFit.cover, ),
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: 
+                const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter username",
+                      labelText: "RIZZLORD",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "PASSWORD",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    onPressed: (){
+                    }, 
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(minimumSize: Size(120, 40)),
+                    )
+                ],
               ),
             ),
-          SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: 
-              const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter username",
-                    labelText: "RIZZLORD",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter password",
-                    labelText: "PASSWORD",
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: (){
-                  }, 
-                  child: Text("Login"),
-                  style: TextButton.styleFrom(),)
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
       );
   }
